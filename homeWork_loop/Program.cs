4b9753047541dc39f3e97047f1fdf8fcc5d25483
LoliDens <104903662+LoliDens@users.noreply.github.com>
2022-10-16 11:57:35 +0500

    Добавьте файлы проекта.

---
+++ homeWork_loop/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homeWork_loop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputUserText;
+            int numberRepeats;
+
+            Console.Write("Введите текст, который должен повторяться: ");
+            inputUserText = Console.ReadLine();
+            Console.Write("Сколько раз текст должен повториться: ");
+            numberRepeats = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < numberRepeats; i++) Console.WriteLine(inputUserText);
+            Console.ReadKey();
+        }
+    }
+}
